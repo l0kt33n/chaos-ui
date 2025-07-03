089e31d5506d73c190e100fad9199c11ad4f729a
l0kt33n <aa77855@gmail.com>
2025-07-02 21:32:48 -0700

    move kitchen sink link to top right

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ const DemoContainer = dynamic(() => import('@/components/demo-container'), { ssr
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="relative flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="/kitchen-sink" className="absolute top-4 right-4 text-blue-500 hover:underline">
+        Go to Kitchen Sink
+      </Link>
       <div className="flex flex-col items-center justify-center space-y-8">
         <DemoContainer />
-        <Link href="/kitchen-sink" className="text-blue-500 hover:underline">
-          Go to Kitchen Sink
-        </Link>
       </div>
     </main>
   )
